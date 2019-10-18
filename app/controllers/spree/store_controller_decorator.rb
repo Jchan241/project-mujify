0b067ffa6b5381422709055828210e0d50b1c6e6
Johnny Chan <johchan24@gmail.com>
2019-10-18 22:54:01 +1100

    fix, buggy store controller decorator

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,21 +1,18 @@
-require 'spree/base_controller'
-require 'spree/store_controller'
-
-Spree::StoreController.class_eval do
-  before_action :load_taxonomies
-
-  def load_taxonomies
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
-  end
-end
-
-# class Spree::StoreController
-#   prepend_before_action :load_taxonomies
+# Spree::StoreController.class_eval do
+#   before_action :load_taxonomies
 
 #   def load_taxonomies
 #     @taxonomies = Spree::Taxonomy.includes(root: :children)
 #   end
 # end
+
+class Spree::StoreController
+  prepend_before_action :load_taxonomies
+
+  def load_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+end
 # module Spree
 #   StoreController.class_eval <<-CODE, __FILE__, __LINE__ + 1
 #   prepend_before_action :load_taxonomies
