5cf57f1189a7a38bd498b16020f24203d829d5b2
Johnny Chan <johchan24@gmail.com>
2019-10-18 22:50:59 +1100

    decorators added back in

---
+++ app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,8 @@
+#needed @taxonomies, unable to inherent fron store controller
+class Spree::ProductsController
+  prepend_before_action :load_taxonomies
+
+  def load_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+end
