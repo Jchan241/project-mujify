5994a9f09293c478fd90b309e4ae6b0aaabf0cdc
Johnny Chan <johchan24@gmail.com>
2019-10-18 20:50:30 +1100

    removing all decorators to test

--- app/controllers/spree/products_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-#needed @taxonomies, unable to inherent fron store controller
-class Spree::ProductsController
-  prepend_before_action :load_taxonomies
-
-  def load_taxonomies
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
-  end
-end
