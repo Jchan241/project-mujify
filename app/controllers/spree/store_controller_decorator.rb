7a062bf17e9d11ddde6ed23cc69abb65d905f4f8
Johnny Chan <johchan24@gmail.com>
2019-10-19 13:13:44 +1100

    fixed decorator bug

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,25 +1,52 @@
-# Spree::StoreController.class_eval do
-#   before_action :load_taxonomies
+# # Spree::StoreController.class_eval do
+# #   before_action :load_taxonomies
 
-#   def load_taxonomies
-#     @taxonomies = Spree::Taxonomy.includes(root: :children)
-#   end
-# end
+# #   def load_taxonomies
+# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
+# #   end
+# # end
 
-class Spree::StoreController
-  prepend_before_action :load_taxonomies
-
-  def load_taxonomies
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
-  end
-end
-# module Spree
-#   StoreController.class_eval <<-CODE, __FILE__, __LINE__ + 1
+# class Spree::StoreController
 #   prepend_before_action :load_taxonomies
 
 #   def load_taxonomies
 #     @taxonomies = Spree::Taxonomy.includes(root: :children)
 #   end
-
-#   CODE
 # end
+# # module Spree
+# #   StoreController.class_eval <<-CODE, __FILE__, __LINE__ + 1
+# #   prepend_before_action :load_taxonomies
+
+# #   def load_taxonomies
+# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
+# #   end
+
+# #   CODE
+# # end
+
+
+# # #needed @taxonomies, unable to inherent fron store controller
+# # class Spree::ProductsController
+# #   prepend_before_action :load_taxonomies
+
+# #   def load_taxonomies
+# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
+# #   end
+# # end
+
+module Spree
+  module StoreControllerDecorator
+    def self.prepended(base)
+        base.before_action :load_taxonomies
+      end
+
+    private
+
+    def load_taxonomies
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
+
+  end
+end
+
+Spree::StoreController.prepend Spree::StoreControllerDecorator
