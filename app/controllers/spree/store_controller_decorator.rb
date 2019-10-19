7d289a4cbfab4328ebb000547793f55e6003cdc9
Johnny Chan <johchan24@gmail.com>
2019-10-19 13:16:48 +1100

    removed old code in helper

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,39 +1,3 @@
-# # Spree::StoreController.class_eval do
-# #   before_action :load_taxonomies
-
-# #   def load_taxonomies
-# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
-# #   end
-# # end
-
-# class Spree::StoreController
-#   prepend_before_action :load_taxonomies
-
-#   def load_taxonomies
-#     @taxonomies = Spree::Taxonomy.includes(root: :children)
-#   end
-# end
-# # module Spree
-# #   StoreController.class_eval <<-CODE, __FILE__, __LINE__ + 1
-# #   prepend_before_action :load_taxonomies
-
-# #   def load_taxonomies
-# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
-# #   end
-
-# #   CODE
-# # end
-
-
-# # #needed @taxonomies, unable to inherent fron store controller
-# # class Spree::ProductsController
-# #   prepend_before_action :load_taxonomies
-
-# #   def load_taxonomies
-# #     @taxonomies = Spree::Taxonomy.includes(root: :children)
-# #   end
-# # end
-
 module Spree
   module StoreControllerDecorator
     def self.prepended(base)
--- app/helpers/base_helper_decorator.rb
+++ app/helpers/base_helper_decorator.rb
@@ -1,21 +1,3 @@
-Spree::BaseHelper.module_eval do
-
-  def taxons_tree(root_taxon, current_taxon, max_level = 1)
-    return '' if max_level < 1 || root_taxon.leaf?
-
-    content_tag :div, class: 'dropdown-menu', 'aria-labelledby' => 'navbarDropdown' do
-      taxons = root_taxon.children.map do |taxon|
-        css_class = current_taxon&.self_and_ancestors&.include?(taxon) ? 'dropdown-item active' : 'dropdown-item'
-        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
-      end
-      safe_join(taxons, "\n")
-    end
-  end
-end
-
-
-
-
 module Spree
   module BaseHelperDecorator
     def taxons_tree(root_taxon, current_taxon, max_level = 1)
