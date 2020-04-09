631ed1cbe29a9874b996c7d6f687d31214c6cef5
Johnny Chan <johnny@Johnnys-MacBook-Pro-2.local>
2020-04-09 20:02:48 +1000

    added aws s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.active_storage.service = :amazon
+
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,6 +17,7 @@ Spree.config do |config|
   config.logo = 'logo/Migu-logo big U.svg'
 end
 
+
 # Configure Spree Dependencies
 #
 # Note: If a dependency is set here it will NOT be stored within the cache & database upon initialization.
@@ -30,3 +31,27 @@ end
 
 
 Spree.user_class = "Spree::User"
+
+
+
+# attachment_config = {
+#   s3_credentials: {
+#     access_key_id:     ENV['AKIA25ZSEP2Q5B7FT2XE'],
+#     secret_access_key: ENV['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'],
+#     bucket:            ENV['arn:aws:s3:::project-mujify']
+#   },
+
+#   storage:        :s3,
+#   s3_region:      ENV['S3_REGION'],
+#   s3_headers:     { "Cache-Control" => "max-age=31557600" },
+#   s3_protocol:    "https",
+#   bucket:         ENV['arn:aws:s3:::project-mujify'],
+#   url:            ":s3_domain_url",
+
+#   path:           "/:class/:id/:style/:basename.:extension",
+#   default_url:    "/:class/:id/:style/:basename.:extension",
+# }
+
+# attachment_config.each do |key, value|
+#   Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+# end
